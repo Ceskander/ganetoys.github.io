421c1e97ba8965cdf910028352816cd0329a3967
Crystal Eskander <ceskander@umail.ucsb.edu>
2020-04-13 21:03:31 -0700

    Background Color

--- my_script.js
+++ my_script.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -20,9 +20,11 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+
+window.onload=function(){
+    openTab(event, 'Home');
+};
